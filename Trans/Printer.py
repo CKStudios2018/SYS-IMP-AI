9ca6f0eeb791c32a1f0cab6f7adc0bd9355a3e90
CKStudios2018 <75259904+CKStudios2018@users.noreply.github.com>
2020-12-04 14:00:04 +0100

    Secondary

---
+++ Trans/Printer.py
@@ -0,0 +1,15 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print("CKStudios")
+print("CKStudios2003")
+print("CKStudios2018")
+print("CKS")
